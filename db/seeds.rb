c9f502d374bdfdefddf941c8431a5db048146e1b
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-08-03 17:36:58 +0500

    intermediate seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Категории
+chemistry = Category.create!( title: 'Химия' )
+informatics = Category.create!( title: 'Информатика' )
+geography = Category.create!( title: 'География' )
+
+# Тесты по химии
+nonmetals = Test.create!( title: 'Неметаллы', level: 3, category_id: chemistry.id )
+oxides = Test.create!( title: 'Оксиды', level: 3, category_id: chemistry.id )
+acids = Test.create!( title: 'Кислоты', level: 3, category_id: chemistry.id )
+
+# Вопросы к тесту по Неметаллам
+question_1 = Question.create!( body: 'Какое из данных веществ используется в медицине, чтобы привести человека в чувства после потери сознания?', test_id: nonmetals.id )
+question_2 = Question.create!( body: 'Какие два химических элемента, основываясь на сходстве их свойств, можно назвать «братьями»?', test_id: nonmetals.id )
+question_3 = Question.create!( body: 'Ковалентная полярная связь присутствует в:', test_id: nonmetals.id )
+question_4 = Question.create!( body: 'Наибольшую степень окисления сера имеет в одном из следующих соединений:', test_id: nonmetals.id )
+ 
+# Ответы на 4 вопрос теста по Неметаллам  
+answer_1 = Answer.create!( body: 'H2SO4', correct: true, question_id: question_4.id )
+answer_2 = Answer.create!( body: 'H2S', correct: false, question_id: question_4.id )
+answer_3 = Answer.create!( body: 'SO2', correct: false, question_id: question_4.id )
+answer_4 = Answer.create!( body: 'K2S', correct: false, question_id: question_4.id )
