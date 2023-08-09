60b53207726170b332c6de6d6b8b1f8a52ceae2a
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-08-09 18:47:06 +0500

    added connections on the model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+  has_many :questions
+  has_many :test_passings
+  has_many :users, through: :test_passings
+
   def self.list_by_category(category_name)
-    joins("JOIN categories ON tests.category_id = categories.id")
+    joins('JOIN categories ON tests.category_id = categories.id')
       .where('categories.title' => category_name)
       .order(title: :desc)
       .pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :test_passings
+  has_many :tests, through: :test_passings
+  has_many :tests, foreign_key: 'author_id'
+
   def tests_by_level(level)
-    Test.joins("JOIN test_passings ON test_passings.test_id = tests.id")
-      .where('test_passings.user_id' => self.id, 'tests.level' => level)
+    Test.joins('JOIN test_passings ON test_passings.test_id = tests.id')
+        .where('test_passings.user_id' => id, 'tests.level' => level)
   end
 end
