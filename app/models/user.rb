76343f15038909935df1d6a24a4b32d2f1603335
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-08-12 09:39:13 +0500

    models and seeds are adjusted for OOP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
-  has_many :test_passings
+  has_many :test_passings, dependent: :destroy
   has_many :tests, through: :test_passings
-  has_many :tests, foreign_key: 'author_id'
+  has_many :created_tests,
+           class_name: 'Test',
+           foreign_key: 'author_id',
+           dependent: :destroy,
+           inverse_of: :author
 
   def tests_by_level(level)
-    Test.joins('JOIN test_passings ON test_passings.test_id = tests.id')
-        .where('test_passings.user_id' => id, 'tests.level' => level)
+    tests.where(level: level)
   end
 end
