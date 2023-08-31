6037162bda66546ef109e2b760bfe22e5a527053
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-08-31 08:26:52 +0500

    change model TestPassings

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,12 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
+  # has_many :test_passings, dependent: :destroy
+  has_many :test_passings,
+           class_name: 'TestPassing',
+           foreign_key: 'current_question_id',
+           dependent: :destroy,
+           inverse_of: :current_question
 
   validates :body, presence: true  
 end
---
+++ db/migrate/20230829022437_add_current_question_to_test_passings.rb
@@ -0,0 +1,7 @@
+class AddCurrentQuestionToTestPassings < ActiveRecord::Migration[6.1]
+  def change
+    add_column :test_passings, :correct_questions_counter, :integer, default: 0, null: false
+    add_reference :test_passings, :current_question, foreign_key: { to_table: :questions }
+  end
+end
+
