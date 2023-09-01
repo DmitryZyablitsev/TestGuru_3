97a2edadd44236a37027de3015f9ed1111f27bfd
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-09-01 15:15:25 +0500

    add rest to tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,12 +1,40 @@
 class TestsController < ApplicationController
-  before_action :set_test, only: %i[show start]
+  before_action :set_test, only: %i[show start destroy, edit update]
 
   def index
    @tests = Test.all
   end
 
+  def new 
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
   def show
     @questions = @test.questions
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
   end
 
   def start
@@ -21,5 +49,7 @@ class TestsController < ApplicationController
     @test = Test.find(params[:id])
   end
 
-
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
 end
