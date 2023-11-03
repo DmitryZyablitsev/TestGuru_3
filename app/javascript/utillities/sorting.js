993871ecae581731b3a461f8c1d80b97830233a5
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-11-03 21:33:00 +0500

    screencast-0.9

--- app/javascript/utillities/sorting.js
+++ app/javascript/utillities/sorting.js
@@ -14,7 +14,16 @@ function sortRowsByTitle() {
   for( let i = 1; i < rows.length; i++ ){
     sortedRows.push(rows[i])
   }
-  sortedRows.sort(compareRows)
+
+  if ( this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+    sortedRows.sort(compareRowsAsc)
+    this.querySelector('.octicon-arrow-up').classList.remove('hide')
+    this.querySelector('.octicon-arrow-down').classList.add('hide')
+  } else {
+    sortedRows.sort(compareRowsDesc)
+    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+    this.querySelector('.octicon-arrow-up').classList.add('hide') 
+  }
 
   let sortedTable = document.createElement('table')
 
@@ -27,11 +36,19 @@ function sortRowsByTitle() {
   table.parentNode.replaceChild(sortedTable, table)
 }
 
-function compareRows(row1, row2) {
+function compareRowsAsc(row1, row2) {
   let testTitle1 = row1.querySelector('td').textContent
   let testTitle2 = row2.querySelector('td').textContent
   if (testTitle1 < testTitle2) { return -1 }
   if (testTitle1 > testTitle2) { return  1 }
+  return 0
+}
+
+function compareRowsDesc(row1, row2) {
+  let testTitle1 = row1.querySelector('td').textContent
+  let testTitle2 = row2.querySelector('td').textContent
+  if (testTitle1 < testTitle2) { return  1 }
+  if (testTitle1 > testTitle2) { return -1 }
   return 0
 }
 
